43ca2be618455895bd42933c42f15c0eb4e6748e
gabrielaguedes12 <gabr1.profissional@gmail.com>
2026-02-15 17:00:51 -0300

    exercicio 3

--- src/main/java/org/example/ScientificCalculator.java
+++ src/main/java/org/example/ScientificCalculator.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class ScientificCalculator {
-    public double add(double a, double b) {
-        return a + b;
+    //public double add(double a, double b) {
+        //return a + b;
+    //}
+
+    public double subtract(double a, double b) {
+        return a - b;
     }
 }
--- src/test/java/org/example/ScientificCalculatorTest.java
+++ src/test/java/org/example/ScientificCalculatorTest.java
@@ -5,14 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ScientificCalculatorTest {
     @Test
-    void testCalcular() {
-        //setup
+     /*void testCalcular() {
+       setup-criação dos objetos, preparação
         ScientificCalculator calculator = new ScientificCalculator();
 
-        //execution
+        execution-execução do método que esta sendo testado
         double result = calculator.add(2,3);
 
-        //assertion
+        assertion-verificação se o resultado é o esperado
         assertEquals(5, result);
+
+        // teardown
+    }*/
+
+    void testSubtract() {
+        /*setup-criação dos objetos, preparação*/
+        ScientificCalculator calculator = new ScientificCalculator();
+
+        /*execution-execução do metodo que esta sendo testado*/
+        double result = calculator.subtract(2,1);
+
+        /*assertion-verificação se o resultado é o esperado*/
+        assertEquals(1, result);
     }
 }
